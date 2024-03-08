373c7dd0fada26eda3ae3777525ad9c772c08e14
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-03-09 06:48:51 +0700

    Add product list and currency exchange feature

--- app/src/main/java/com/arkan/aresto/MainActivity.kt
+++ app/src/main/java/com/arkan/aresto/MainActivity.kt
@@ -6,14 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.arkan.aresto.adapter.CategoryAdapter
+import com.arkan.aresto.adapter.ProductAdapter
 import com.arkan.aresto.databinding.ActivityMainBinding
 import com.arkan.aresto.model.Category
+import com.arkan.aresto.model.Product
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
     private val categoryAdapter = CategoryAdapter()
+    private val productAdapter = ProductAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         setListCategory()
+        setListProduct()
     }
     private fun setListCategory() {
         val dataCategory = listOf(
@@ -36,5 +40,19 @@ class MainActivity : AppCompatActivity() {
             adapter = this@MainActivity.categoryAdapter
         }
         categoryAdapter.submitData(dataCategory)
+    }
+    private fun setListProduct() {
+        val dataProduct = listOf(
+            Product(image = R.drawable.img_grilled_chicken, name = "Ayam Bakar", price = 50000.00),
+            Product(image = R.drawable.img_fried_chicken, name = "Ayam Goreng", price = 40000.00),
+            Product(image = R.drawable.img_geprek_chicken, name = "Ayam Geprek", price = 40000.00),
+            Product(image = R.drawable.img_chicken_guts_satay, name = "Sate Usus Ayam", price = 5000.00),
+            Product(image = R.drawable.img_fried_rice, name = "Nasi Goreng", price = 20000.00),
+            Product(image = R.drawable.img_fried_noodle, name = "Mie Goreng", price = 15000.00),
+        )
+        binding.layoutProduct.rvProduct.apply {
+            adapter = this@MainActivity.productAdapter
+        }
+        productAdapter.submitData(dataProduct)
     }
 }
