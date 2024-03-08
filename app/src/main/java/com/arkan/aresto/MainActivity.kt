0cf821f5078611fa3451abf0839cc39c77c6b6e0
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-03-09 06:43:28 +0700

    Add Category list

--- app/src/main/java/com/arkan/aresto/MainActivity.kt
+++ app/src/main/java/com/arkan/aresto/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.arkan.aresto.adapter.CategoryAdapter
 import com.arkan.aresto.databinding.ActivityMainBinding
+import com.arkan.aresto.model.Category
 
 class MainActivity : AppCompatActivity() {
     private val binding : ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val categoryAdapter = CategoryAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +23,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setListCategory()
+    }
+    private fun setListCategory() {
+        val dataCategory = listOf(
+            Category(image = R.drawable.img_category_rice, name = "Nasi"),
+            Category(image = R.drawable.img_category_noodle, name = "Mie"),
+            Category(image = R.drawable.img_category_snack, name = "Snack"),
+            Category(image = R.drawable.img_category_drink, name = "Minuman")
+        )
+        binding.rvCategory.apply {
+            adapter = this@MainActivity.categoryAdapter
+        }
+        categoryAdapter.submitData(dataCategory)
     }
 }
