ffa6ea767bdafbe3ada9a726a0d04a5faeb03ba4
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-03-19 14:01:52 +0700

    Add Detail Activity

---
+++ app/src/main/java/com/arkan/aresto/presentation/activity/DetailActivity.kt
@@ -0,0 +1,26 @@
+package com.arkan.aresto.presentation.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.arkan.aresto.R
+import com.arkan.aresto.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
